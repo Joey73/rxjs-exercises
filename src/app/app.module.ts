8414eb9784f9af6d29276b84a11ee36ea372bfee
Joerg Schmitz <schmitz873@gmail.com>
2022-12-22 16:46:09 +0100

    Several improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
 import { ObservablesComponent } from './observables/observables.component';
 import { OperatorsComponent } from './operators/operators.component';
-import { MatButtonModule } from '@angular/material/button';
+import { Test1Component } from './observables/test1/test1.component';
+import { Test2Component } from './observables/test2/test2.component';
+import { Test3Component } from './observables/test3/test3.component';
 
 @NgModule({
   declarations: [
@@ -17,14 +21,18 @@ import { MatButtonModule } from '@angular/material/button';
     MenuComponent,
     HomeComponent,
     ObservablesComponent,
-    OperatorsComponent
+    OperatorsComponent,
+    Test1Component,
+    Test2Component,
+    Test3Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
