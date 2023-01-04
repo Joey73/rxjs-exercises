b6e4ef11f2fdbe5a853f1593a7113e07693c9a5a
Joerg Schmitz <schmitz873@gmail.com>
2023-01-04 23:51:51 +0100

    Implementation of tasks deleted

--- src/app/operators/operators.component.ts
+++ src/app/operators/operators.component.ts
@@ -32,35 +32,14 @@ export class OperatorsComponent implements OnDestroy {
     this.subscription1 = this.randomNumberObservable$.subscribe(this.randomNumberSubject$);
 
     // Übrung 1: Subscribe randomNumberSubject$ und fülle task1ResultString
-    this.subscription2 = this.randomNumberSubject$.subscribe({
-      next: data => this.task1ResultString += `-----${data}`,
-      error: e => console.log(e),
-    });
     
     // Übung 2: Verdopple jeden Wert und speichere diese in task2ResultString.
-    this.subscription3 = this.randomNumberSubject$.pipe(
-      map(x => x * 2)
-    ).subscribe({
-      next: data => this.task2ResultString += `-----${data}`,
-      error: e => console.log(e),
-    });
 
     // Übung 3: Verdopple jeden Wert und speichere alle die > 100 sind in task3ResultString.
-    this.subscription4 = this.randomNumberSubject$.pipe(
-      map(x => x * 2),
-      filter(x => x > 100)
-    ).subscribe({
-      next: data => this.task3ResultString += `-----${data}`,
-      error: e => console.log(e),
-    });
   }
 
   ngOnDestroy(): void {
     // Übung 4: Alle Subscriptions unsubscriben
-    this.subscription1?.unsubscribe();
-    this.subscription2?.unsubscribe();
-    this.subscription3?.unsubscribe();
-    this.subscription4?.unsubscribe();
   }
 
 }
